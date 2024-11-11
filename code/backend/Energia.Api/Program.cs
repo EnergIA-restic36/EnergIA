cd8a25a9a18881117ac8d2b1899fba36af58972a
fredso82 <fredso82@gmail.com>
2024-11-11 15:37:18 -0300

    configuração para autenticação

--- code/backend/Energia.Api/Program.cs
+++ code/backend/Energia.Api/Program.cs
@@ -1,9 +1,33 @@
 using Energia.Api.Context;
 using Energia.Api.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<EnergiaDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<EnergiaDbContext>()
+    .AddDefaultTokenProviders();
+
+var key = Encoding.ASCII.GetBytes("OGynApWowsX1sT8vSYfELf6kVxgjoe9V");
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services.AddScoped<AmbienteRepository>();
 builder.Services.AddScoped<ConsumoRepository>();
@@ -30,17 +54,16 @@ using (var scope = app.Services.CreateScope())
     dbContext.Database.EnsureCreated();
 }
 
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseCors();
+app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
