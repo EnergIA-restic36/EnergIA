cc950e2e408c1db5691a691407d9d98a4ce73ed3
fredso82 <fredso82@gmail.com>
2024-11-23 21:18:04 -0300

    Conexão com postgres

--- code/backend/Energia.Api/Repositories/ConsumoRepository.cs
+++ code/backend/Energia.Api/Repositories/ConsumoRepository.cs
@@ -5,6 +5,10 @@ using Microsoft.EntityFrameworkCore;
 namespace Energia.Api.Repositories
 {
     public class ConsumoRepository(EnergiaDbContext context) : RepositoryBase<Consumo>(context)
-    {       
+    {      
+        public async Task<IEnumerable<Consumo>> ObterPorDispositivo(string dispositivoId)
+        {
+            return await _context.Consumos.Where(c => c.DispositivoId == dispositivoId).ToListAsync();  
+        }
     }
 }
